1c8c9fdc12397a1b6fecf36812dfb47bd1747856
eviejennings <evie_jennings@outlook.com>
2023-04-28 16:14:16 +0100

    Icon settings

--- src/app.js
+++ src/app.js
@@ -29,12 +29,19 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "ae4fe30b0a36b3of8f1b54a6ecf83t04";
